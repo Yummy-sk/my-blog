938da95701076ff76d3213a90e23da18651418d0
Sang kwon Yeum <kunshup2000@gmail.com>
2023-01-21 07:09:29 +0900

    init: lint 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'www.notion.so',
+    ],
+  },
+  webpack: config => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig
