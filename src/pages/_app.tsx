da217747ebe34e4ce2f4404319d6573e2581d72f
Sang kwon Yeum <kunshup2000@gmail.com>
2023-01-21 09:55:55 +0900

    feat: font 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import type { AppProps } from 'next/app';
+import { Roboto } from '@next/font/google';
 import Provider from '@/provider';
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
+    <main className={roboto.className}>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </main>
   );
 }
