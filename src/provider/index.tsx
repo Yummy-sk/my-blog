2c62d265a72c528d30771df8948f7a11566956c1
Sang kwon Yeum <kunshup2000@gmail.com>
2023-01-23 11:02:35 +0900

    feat: 다크모드 전환 시, transition 추가

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -1,5 +1,5 @@
 import { setConfig } from 'cloudinary-build-url';
-import { ChakraProvider, VStack } from '@chakra-ui/react';
+import { ChakraProvider, VStack, extendTheme } from '@chakra-ui/react';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyle } from '@/styles';
 
@@ -8,9 +8,23 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   });
 
+  const ctheme = extendTheme({
+    styles: {
+      global: {
+        body: {
+          transitionProperty: 'all',
+          transitionDuration: '0.8s',
+        },
+      },
+    },
+    config: {
+      disableTransitionOnChange: false,
+    },
+  });
+
   return (
     <VStack h='100vh' justifyContent='center'>
-      <ChakraProvider>
+      <ChakraProvider theme={ctheme}>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
           {children}
