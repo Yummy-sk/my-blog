2bc76669bf11e77ab1b43042d50987ce32c156eb
Sang kwon Yeum <kunshup2000@gmail.com>
2023-01-28 05:56:00 +0900

    feat: not found 페이지 구성

--- src/components/post_list/index.tsx
+++ src/components/post_list/index.tsx
@@ -1,5 +1,6 @@
 import { PostOption } from '@/components/post_list/post_option/PostOption';
 import { PostSection } from '@/components/post_list/post_section/PostSection';
+import { PostNotFound } from '@/components/post_list/post_not_found/PostNotFound';
 import { Transition } from '@/common';
 import { PostListProps } from '@/types/data';
 import * as S from './style';
@@ -15,9 +16,11 @@ export function PostList({ posts, keyword, onChangeKeyword }: Props) {
     <S.Container>
       <Transition>
         <PostOption keyword={keyword} onChangeKeyword={onChangeKeyword} />
-        {posts.map(post => (
-          <PostSection key={post.id} post={post} />
-        ))}
+        {posts.length ? (
+          posts.map(post => <PostSection key={post.id} post={post} />)
+        ) : (
+          <PostNotFound keyword={keyword} />
+        )}
       </Transition>
     </S.Container>
   );
--- src/components/post_list/post_not_found/PostNotFound.style.ts
+++ src/components/post_list/post_not_found/PostNotFound.style.ts
@@ -2,8 +2,16 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  flex: 1;
 
   display: flex;
   flex-direction: column;
+  align-items: center;
+
+  margin-top: 3rem;
+
+  p {
+    font-size: 1rem;
+    font-weight: 800;
+  }
 `;
