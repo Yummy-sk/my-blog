ad6ba9b26f0cbb8aeb0626db578c95824c1c459c
Sang kwon Yeum <kunshup2000@gmail.com>
2023-01-28 06:10:05 +0900

    feat: 에러 페이지 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Home } from '@/components/home';
 export { PostList } from '@/components/post_list';
 export { Post } from '@/components/post';
 export { Contact } from '@/components/contact';
+export { Error } from '@/components/error';
