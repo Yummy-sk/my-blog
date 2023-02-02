e20b4cd2d76664d093275b4184591ff2eb412d61
Sang kwon Yeum <kunshup2000@gmail.com>
2023-02-02 16:35:37 +0900

    fix: add lazy motion

--- src/common/Transition/index.tsx
+++ src/common/Transition/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { LazyMotion, motion, domAnimation } from 'framer-motion';
 
 export function Transition({ children }: { children: React.ReactNode }) {
   return (
-    <motion.div
-      variants={{
-        hidden: { opacity: 0 },
-        enter: { opacity: 1 },
-        exit: { opacity: 0 },
-      }}
-      initial='hidden'
-      animate='enter'
-      exit='exit'
-      transition={{ type: 'linear' }}
-      style={{
-        width: '100%',
-      }}>
-      {children}
-    </motion.div>
+    <LazyMotion features={domAnimation}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0 },
+          enter: { opacity: 1 },
+          exit: { opacity: 0 },
+        }}
+        initial='hidden'
+        animate='enter'
+        exit='exit'
+        transition={{ type: 'linear' }}
+        style={{
+          width: '100%',
+        }}>
+        {children}
+      </motion.div>
+    </LazyMotion>
   );
 }
