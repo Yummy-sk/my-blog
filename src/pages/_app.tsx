e932e614690bd09a79c0c3e8162f78d3cd57dbbe
Sang kwon Yeum <kunshup2000@gmail.com>
2023-02-02 21:20:44 +0900

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = dynamic(() => import('@/common/Layout').then(mod => mod.Layout));
 
@@ -7,10 +8,13 @@ const Provider = dynamic(() => import('@/provider'));
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Provider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+      <Analytics />
+    </>
   );
 }
