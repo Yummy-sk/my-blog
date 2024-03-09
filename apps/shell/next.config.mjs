8bbfd296e8d0cb99fe4ac2e89e53577599a94656
Yummy-sk <kunshup2000@gmail.com>
2024-03-09 14:30:57 +0900

    feat: app shell에 ui-kit 가져오기

--- apps/shell/next.config.mjs
+++ apps/shell/next.config.mjs
@@ -1,6 +1,24 @@
+import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'shell',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {},
+        shared: {
+          '@career-up/ui-kit': {
+            singleton: true,
+          },
+        },
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ packages/ui-kit/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    outDir: './dist',
+    lib: {
+      entry: './src/index.ts',
+      name: 'ui-kit',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
