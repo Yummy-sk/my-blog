ce2154daf491798e2a879a482b034b703742d670
Yummy-sk <kunshup2000@gmail.com>
2024-03-10 13:25:36 +0900

    feat: 컴포넌트 동적 생성

--- apps/about/src/injector.tsx
+++ apps/about/src/injector.tsx
@@ -2,11 +2,12 @@ import App, { AppProps } from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-export const inject = (parentElementId: string, props: AppProps) => {
-  const root = createRoot(document.getElementById(parentElementId)!);
-  root.render(<App {...props} />);
+export const Comps = () => {
+  // const root = createRoot(document.getElementById(parentElementId)!);
+  // root.render(<App {...props} />);
 
-  return () => {
-    root.unmount();
-  };
+  // return () => {
+  //   root.unmount();
+  // };
+  return <App name="hello" />;
 };
--- apps/shell/src/pages/index.tsx
+++ apps/shell/src/pages/index.tsx
@@ -1,23 +1,21 @@
 import { Button } from '@my-site/ui-kit';
 import { useEffect } from 'react';
 
+const Comps = dynamic(() => import('about/injector').then((mod) => mod.Comps), {
+  ssr: false,
+});
+
+import dynamic from 'next/dynamic';
+
 const elementId = 'isolated-app';
 
 export default function Home() {
-  useEffect(() => {
-    let unmount: () => void = () => {};
-    import('about/injector').then(({ inject }) => {
-      unmount = inject(elementId, { name: 'main' });
-    });
-    return () => {
-      unmount();
-    };
-  }, []);
-
   return (
     <main>
       <Button />
-      <div id={elementId}></div>
+      {/* <div id={elementId}></div> */}
+
+      <Comps />
     </main>
   );
 }
